a9ba9d341e735737549632ff93b10b1b52231409
Joseph Dasilva <jojods1125@gmail.com>
2021-01-15 17:09:32 -0500

    Added credit UI number

---
+++ Assets/Scripts/UICredits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UICredits : MonoBehaviour
+{
+
+    // FixedUpdate is called once per fixed time
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<TMP_Text>().text = GameManager.instance.numCredits.ToString();
+    }
+}
