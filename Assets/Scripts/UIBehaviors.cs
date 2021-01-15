917280c48e23e7731520458bb7b93ee60a8fb18b
Joseph Dasilva <jojods1125@gmail.com>
2021-01-14 21:53:10 -0500

    How to Play screen finished, buttons working. Weird black screen bug

---
+++ Assets/Scripts/UIBehaviors.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIBehaviors : MonoBehaviour
+{
+    public void ActivateObject(GameObject other)
+    {
+        other.SetActive(true);
+    }
+
+    public void DeactivateObject(GameObject other)
+    {
+        other.SetActive(false);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
